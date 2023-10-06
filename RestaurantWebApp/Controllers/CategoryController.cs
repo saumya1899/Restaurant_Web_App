69f074378e5577fec616bf9c4db4be2faf7c6ac6
Saumya Patel <saumyapatel529@gmail.com>
2023-10-06 18:17:06 -0400

    Added 4 different screens. Linked with main screen

---
+++ RestaurantWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantWebApp.Controllers;
+
+
+
+
+    public class CategoryController : Controller
+    {
+        public IActionResult Appetizers()
+        {
+            return View();
+        }
+
+    public IActionResult Beverages()
+    {
+        return View();
+    }
+
+    
+
+        public IActionResult Desserts()
+        {
+            return View();
+        }
+
+    public IActionResult MainCourses()
+    {
+        return View();
+    }
+
+}
+
+
+
